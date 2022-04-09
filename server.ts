2fa4fa44610e258e2af04b954ad5cb77441a590b
ZWBruce <846235584@qq.com>
2022-04-09 23:32:31 +0800

    feat: mock file upload

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
-const http = require('http');
+// const http = require('http');
+import {createServer} from 'http';
+import { createWriteStream, writeFileSync } from 'fs';
+import {parse} from 'querystring'
 
-http.createServer((req, res) => {
+createServer((req, res) => {
   console.log(req.url);
   if(req.url === '/set-cookie') {
     res.setHeader('Set-Cookie', 'name=value;path=/');
@@ -8,7 +11,23 @@ http.createServer((req, res) => {
   }else if(req.url === '/set-cookie1') {
     res.setHeader('Set-Cookie', 'name=sss;key=value; SameSite=None; Secure');
     res.end('Cookie set');
-  }else {
+  }else if(req.url === '/upload') {
+    console.log(req.headers);
+    // plan 1
+    // let data: Uint8Array[] = [];
+    // req.on('data', chunk => {
+    //   data.push(chunk);
+    // });
+    // req.on('end', () => {
+    //   const realData = Buffer.concat(data);
+    //   writeFileSync('./upload.jpg', realData);
+    //   res.end(realData);
+    // })
+    // plan 2 (best practice)
+    req.pipe(createWriteStream('./upload.jpg'));
+    res.end('uploaded');
+  }
+  else {
     res.end('Hello');
   }
 }).listen(2022);
--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,42 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import './App.css'
+import './App.css';
 import workerPort from './util/worker';
 
 const { port1, port2 } = new MessageChannel();
 
 function App() {
-
   const [count, setCount] = useState(0);
-  const [value, setValue] = useState('')
-  const [list, setList] = useState([])
+  const [value, setValue] = useState('');
+  const [list, setList] = useState([]);
   const iframeRef = useRef<HTMLIFrameElement>(null);
-  const divRef = useRef<HTMLDivElement>(null)
+  const divRef = useRef<HTMLDivElement>(null);
+  const fileRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     // (0, eval)(`import _ from 'lodash'`);
-
-    const shadowDom = divRef.current!.attachShadow({ mode: 'open' });
-    ReactDOM.render(
-      <>
-        <p part="p">p in shadowDom with part</p>
-        <p>p in shadowDom</p>
-      </>, 
-      shadowDom
-    );
+    if (!divRef.current?.shadowRoot) {
+      const shadowDom = divRef.current!.attachShadow({ mode: 'open' });
+      ReactDOM.render(
+        <>
+          <p part="p">p in shadowDom with part</p>
+          <p>p in shadowDom</p>
+        </>,
+        shadowDom
+      );
+    }
 
     workerPort.addEventListener('message', (e: MessageEvent) => {
-      console.log(e)
-      setList(e.data)
+      console.log(e);
+      setList(e.data);
     });
-    
-    
+
     return () => {
-      workerPort.postMessage('close00')
-      workerPort.close()
-      workerPort.postMessage('close')
-    }
-  }, [])
+      workerPort.postMessage('close00');
+      workerPort.close();
+      workerPort.postMessage('close');
+    };
+  }, []);
 
   return (
     <div>
@@ -47,34 +47,75 @@ function App() {
             count is: {count}
           </button>
 
-          <button onClick={ () => {
-            // fetch('/api/ifram', { method: 'post' });
+          <button
+            onClick={() => {
+              fetch('/api/default').then(async (res) =>
+                console.log(await res.text())
+              );
 
-            console.log(iframeRef.current?.contentWindow?.document);
-          } }>req</button>
+              // console.log(iframeRef.current?.contentWindow?.document);
+            }}
+          >
+            req
+          </button>
         </p>
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
+        <input
+          type="file"
+          name="abc"
+          id="file"
+          ref={fileRef}
+          onChange={(e) => {
+            console.log(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            const file = fileRef.current?.files?.[0];
+
+            if (file) {
+              fetch('/api/upload', {
+                method: 'post',
+                body: file,
+                headers: {
+                  'Content-Type': 'multipart/form-data',
+                },
+              }).then(async (res) => console.log(await res.text()));
+            }
+          }}
+        >
+          upload
+        </button>
         <div ref={divRef} className="shadow-con"></div>
-        <input type="text" name="" id="" onChange={e => {
-          setValue(e.target.value)
-        }} />
-        <button onClick={() => {
-          if(!value) {
-            return ;
-          }
-          workerPort.postMessage(value)
-        }}>push data</button>
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            if (!value) {
+              return;
+            }
+            workerPort.postMessage(value);
+          }}
+        >
+          push data
+        </button>
 
         <ul>
-        {
-          list.map(t => <li key={t}>{t}</li>)
-        }
-</ul>
+          {list.map((t) => (
+            <li key={t}>{t}</li>
+          ))}
+        </ul>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
